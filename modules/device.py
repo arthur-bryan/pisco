141ddd6779ec2554b22e29ce746d9796d79f51b3
arthur-bryan <arthurbryan20303@gmail.com>
2020-08-08 04:51:35 +0200

    added ssh and multiple devices support

--- modules/device.py
+++ modules/device.py
@@ -9,7 +9,7 @@ FILES_FOLDER = os.path.join(CURRENT_DIR, "../files")
 
 class Device:
 
-    def __init__(self, device_type, ip_address, vty_username, vty_password, enable_secret):
+    def __init__(self, device_type, ip_address, vty_username, vty_password, enable_secret, domain_name):
         self.device_type = device_type
         self.ip_address = ip_address
         self.vty_username = vty_username
@@ -17,24 +17,32 @@ class Device:
         self.enable_secret = enable_secret
         self.interfaces = []
         self.is_configured = False
+        if domain_name == "":
+            self.domain_name = 'lan.com'
+        else:
+            self.domain_name = domain_name
 
-    def export_interfaces_info(self, option, manager):
+    def export_interfaces_info(self, option, connection_method, client_obj):
         """ Stores the interfaces status command output to a txt and then
             create a csv based on it.
             Params:
                 option (str):  must be 'status' or 'ip'.
-                manager (class Manager): the object manager that manages the device.
+                connection_method (str): 'telnet' or 'ssh'.
+                client_obj: the telnet or ssh client to send commands to device.
             Returns:
                 csv_interface_file
         """
-        if option == 'status':
-            command = 'show interfaces status\n'
-        else:
-            command = 'show ip interface brief\n'
-        manager.write(command.encode())
-        sleep(0.5)
-        output = manager.read_very_eager().decode('ascii')
-        # opens a .txt and writes the 'show interfaces status' output to it.
+        option_commands = {'status': 'show interfaces status\n',
+                           'ip': 'show ip interface brief\n'}
+        if connection_method == 'telnet':
+            client_obj.write(option_commands[option].encode())
+            sleep(0.5)
+            output = client_obj.read_very_eager().decode('ascii')
+        elif connection_method == 'ssh':
+            client_obj.send(option_commands[option].encode())
+            sleep(0.5)
+            output = client_obj.recv(65535).decode('ascii')
+        # writes output to a .txt file.
         with open(f"{FILES_FOLDER}/interface.txt", "w") as file:
             if option == "ip":
                 file.write("\n")    # blank line to help formating file on this output
@@ -48,12 +56,13 @@ class Device:
             file.seek(0)
             lines = []
             for i, line in enumerate(file.readlines()):
-                if i == 2 and i < number_of_lines - 1:   # ignores the firsts and last useless lines.
+                if i == 2 and i < number_of_lines - 1:   # ignores the 2 firsts and last useless lines.
                     line = line.split()
                     lines.append(line)
                 elif 2 < i < number_of_lines - 1:
                     line = line.split()
-                    line.insert(1, "")
+                    if option == "status" and len(line) < 7:
+                        line.insert(1, "")
                     lines.append(line)
             csv_writer.writerows(lines)
             csv_interface_file.close()
